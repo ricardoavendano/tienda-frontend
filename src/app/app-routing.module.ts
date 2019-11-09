cb59cb98d6bcddd2459a68fd31bcc18e7d22c8da
ricardoavendano <ricardoav543@gmail.com>
2019-11-08 19:09:56 -0500

    crear modulo libro

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { LogoutComponent } from './logout/logout.component';
 import { AuthGaurdService } from './service/auth-gaurd.service';
+import { LibrosListComponent } from "./libro/libro.component";
+
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'logout', component: LogoutComponent,canActivate:[AuthGaurdService] },
+  { path: 'libro-list', component: LibrosListComponent,canActivate:[AuthGaurdService] },
 ];
 
 @NgModule({
